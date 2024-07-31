1cd7cab689f267eb7c38003453bbe740642b00d7
Gokul6381 <148840904+Gokul6381@users.noreply.github.com>
2024-07-31 03:59:07 +0000

    code added

--- weather.py
+++ weather.py
@@ -1,1 +1,6 @@
-print('hello')
+import os
+from langchain_community.utilities import OpenWeatherMapWrapper
+os.environ["OPEN_WEATHERMAP _API_KEY"]="a7708f9d88d41e367c132b1b500c37f9"
+weather = OpenWeatherMapWrapper(api_key=os.environ["OPEN_WEATHERMAP_API_KEY"])
+result=weather.run('ooty')
+print(result)
